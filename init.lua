284a31350ba9f27a7ce9b98c1007f83c2e5bd2ac
Vegard <vegsei@gmail.com>
2023-05-25 09:17:48 +0200

    require theme

--- init.lua
+++ init.lua
@@ -5,6 +5,8 @@
 ------- options:
 require "vegard.base.options"
 require "vegard.base.keymaps"
+require "vegard.base.plugins"
+require "vegard.base.theme"
 if vim.g.vscode then
     -- VSCode extension
 else
