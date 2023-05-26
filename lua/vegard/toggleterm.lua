2600fdc73071f8fc9ce5732973d6da5656e15990
Vegard <vegsei@gmail.com>
2023-05-26 12:18:21 +0200

    modified toggleterm
    Added lazygit functionality

--- lua/vegard/toggleterm.lua
+++ lua/vegard/toggleterm.lua
@@ -25,3 +25,9 @@ toggleterm.setup({
 		},
 	},
 })
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _LAZYGIT_TOGGLE()
+	lazygit:toggle()
+end
