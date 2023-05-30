77417a257954cf0ed6048d7c7fff45d9556d39a8
Vegard <vegsei@gmail.com>
2023-05-30 08:18:46 +0200

    added whichkey to organize keybinds

--- lua/vegard/core/keymaps.lua
+++ lua/vegard/core/keymaps.lua
@@ -45,7 +45,42 @@ keymap("v", "<C-u>", "<C-u>zz>", opts)
 --============================================
 -----------------Plugins----------------------
 --============================================
+local ok, wk = pcall(require, "which-key")
+if not ok then
+  return
+end
 
+wk.register({
+  q = {"<cmd>q<cr>", "Exit"},
+  w = {"<cmd>w<cr>", "Write"},
+  t = {
+    name = "Tabs",
+    n = {"<cmd>tabnew<cr>", "New Tab"},
+    l = {"<cmd>tabNext<cr>", "Next Tab"},
+    h = {"<cmd>tabPrevious<cr>", "Previous Tab"},
+    q = {"<cmd>tabclose<cr>", "Close Tab"}
+  },
+  s = {
+    name = "Split",
+    h = {"<cmd>vertical split<cr>", "Vertical Split"},
+    v = {"<cmd>split<cr>", "Horizontal Split"}
+  },
+  g = {
+    name = "Git",
+    g = {":lua _LAZYGIT_TOGGLE()<cr>", "Lazygit"},
+    k = {":Gitsigns prev_hunk<cr>", "Previous Hunk"},
+    j = {":Gitsigns next_hunk<cr>", "Next Hunk"},
+    d = {":Gitsigns diffthis<cr>", "Diff"},
+    l = {":Gitsigns preview_hunk<cr>", "Preview"},
+    a = {":Gitsigns stage_hunk<cr>", "Stage Hunk"},
+    A = {":Gitsigns stage_buffer<cr>", "Stage Buffer"}
+  }
+}, {prefix = "<leader>" })
+
+
+---- New windows
+keymap("n", "<leader>sh", ":vertical split<CR>", opts)
+keymap("n", "<leader>sv", ":split<CR>", opts)
 -- Open NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 -- open lazygit:
