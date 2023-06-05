8acb07a0aa65c0e7dd9c5cb24d0e90cbaab8eb8f
Vegard <vegsei@gmail.com>
2023-06-05 06:15:38 +0200

    changed theme to mocchiato

--- lua/vegard/plugins/theme.lua
+++ lua/vegard/plugins/theme.lua
@@ -1,9 +1,9 @@
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+--    background = { -- :h background
+--        light = "latte",
+--        dark = "mocha",
+--    },
     transparent_background = false,
     show_end_of_buffer = false, -- show the '~' characters after the end of buffers
     term_colors = false,
